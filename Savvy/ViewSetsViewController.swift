0da304c2f721c56f257547e77118d45f51b7cab4
Cody Rhoads <codyrhoads@gmail.com>
2016-02-11 21:35:56 -0800

    Added Study Set View controller, and updated some segue functionality with ViewSetsViewController

--- Savvy/ViewSetsViewController.swift
+++ Savvy/ViewSetsViewController.swift
@@ -10,4 +10,16 @@ import UIKit
 
 class ViewSetsViewController: UIViewController {
     
+    @IBOutlet weak var studySetButton: UIButton!
+    @IBOutlet weak var editSetButton: UIButton!
+    
+    @IBAction func studySet(sender: AnyObject) {
+        print("Touched Study Set button")
+        performSegueWithIdentifier("studySetSegue", sender: sender)
+    }
+    
+    @IBAction func editSet(sender: AnyObject) {
+        print("Touched Edit Set button")
+        performSegueWithIdentifier("editSetSegue", sender: sender)
+    }
 }
