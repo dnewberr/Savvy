e14c79b2a9042a968e4dd620296cfe540b75ebf2
Cody Rhoads <codyrhoads@gmail.com>
2016-02-29 19:18:33 -0800

    Added flashcards collection support to the home view. No actual flashcard data yet.

--- Savvy/HomeViewController.swift
+++ Savvy/HomeViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
+class CardCollectionViewCell: UICollectionViewCell {
+    var flashcard: Flashcard?
+    @IBOutlet weak var label: UILabel!
+}
+
+class HomeViewController: UIViewController, FBSDKLoginButtonDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     @IBOutlet weak var createSetButton: UIButton!
     @IBOutlet weak var viewSetsButton: UIButton!
     @IBOutlet weak var importButton: UIButton!
-    
     @IBOutlet weak var logoutButton: UIButton!
+    
+    // Collection of flashcards
+    @IBOutlet weak var flashcardCollection: UICollectionView!
+    
+    // Array to hold flashcards
+    var cardArray = Array<Flashcard>()
     
     @IBAction func viewBadges(sender: AnyObject) {
         performSegueWithIdentifier("homeToBadges", sender: sender)
     }
+    
     @IBAction func createSet(sender: AnyObject) {
         let nextScene = self.storyboard?.instantiateViewControllerWithIdentifier("Create") as! CreateSetController
         nextScene.prevSceneId = "Home"
@@ -42,8 +53,15 @@ class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
         
         performSegueWithIdentifier("homeToLogin", sender: sender)
     }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()    }
+        super.viewDidLoad()
+        // Populates array with flashcards for testing purposes
+        for (var i = 0; i < 5; i++) {
+            cardArray.append(Flashcard(newTerm: String(i), newDef: "The def is " + String(i * 2), newDue: 0))
+        }
+        setUpFlashcardCollection()
+    }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
         
@@ -56,5 +74,49 @@ class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // Where we would presumably populate the flashcard array.
+    func setUpFlashcardCollection() {
+        flashcardCollection.dataSource = self
+        flashcardCollection.delegate = self
+    }
+    
+    // Returns how many cards are on the home screen (aka the length of the card array).
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    // Sets up the cells. The terms are shown initially.
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cardCell", forIndexPath: indexPath) as! CardCollectionViewCell
+        let card = cardArray[indexPath.row]
+        cell.flashcard = card
+        if card.termSide == true {
+            cell.label.text = card.term
+        }
+        else {
+            cell.label.text = card.definition
+        }
+        return cell
+    }
+
+    // Switches between the term and definition when the flashcard is tapped.
+    @IBAction func tappedCell(sender: AnyObject) {
+        let tappedPoint = sender.locationInView(self.flashcardCollection)
+        let tappedCellPath = flashcardCollection.indexPathForItemAtPoint(tappedPoint)
+        if let tappedCellPath = tappedCellPath {
+            let cell = flashcardCollection.cellForItemAtIndexPath(tappedCellPath) as! CardCollectionViewCell
+            if let card = cell.flashcard {
+                if card.termSide == true {
+                    cell.label.text = card.definition
+                    card.termSide = false
+                }
+                else {
+                    cell.label.text = card.term
+                    card.termSide = true
+                }
+            }
+        }
     }
 }
